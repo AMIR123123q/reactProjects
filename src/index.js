85982506bba5059721e55e8b53e50013e563e6b9
amir <amir.nooroz.mobarak@gmail.com>
2022-11-22 13:15:23 +0330

    exsiarsize

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
-ReactDOM.render(<BrowserRouter><App/></BrowserRouter>,document.getElementById("root"))
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 registerServiceWorker();
-
-
-
-
-
-
-
-
-
-
-
-
